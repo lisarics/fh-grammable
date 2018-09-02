b79685f1051f105207a7c3b64895c8502cd32d77
lisarics <lisa.richardson@gmail.com>
2018-09-02 19:45:43 +0000

    added a detail page for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,13 @@ before_action :authenticate_user!, only: [:new, :create]
   def index
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
