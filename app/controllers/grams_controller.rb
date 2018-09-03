b4c1760b7e2407052098dc4fd7c78db0d8c79a05
lisarics <lisa.richardson@gmail.com>
2018-09-03 18:06:45 +0000

    added support for comments

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -56,8 +56,4 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   def gram_params
     params.require(:gram).permit(:message, :picture)
   end
-
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
 end
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,7 @@
 class Gram < ApplicationRecord
   validates :message, presence: true
-  belongs_to :user
+  validates :picture, presence: true 
+  has_many :comments
   mount_uploader :picture, PictureUploader
+  belongs_to :user
 end
